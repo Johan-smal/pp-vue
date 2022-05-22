fbb5a83784025a28c56b4e2840796f17d288966c
Johan Smal <johansmal@MacBook-Air.local>
2022-05-22 20:25:59 +0200

    tailwind setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,7 @@
 <template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   <router-link :to="{ name: 'users' }"><button>GITHUB USERS</button></router-link>
 </template>
 <script setup lang="ts"></script>
